0a5f1177a490c146da1456402fd31234656a2065
vonThule <alexpopp82@gmail.com>
2018-01-07 20:45:41 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,27 @@
 var socket = io();
 
+function scrollToBottom() {
+
+		// Selectors
+		var messages = jQuery('#messages');
+		var newMessage = messages.children('li:last-child');
+
+		// Heights
+		var clientHeight = messages.prop('clientHeight');
+		var scrollTop = messages.prop('scrollTop');
+		var scrollHeight = messages.prop('scrollHeight');
+		var newMessageHeight = newMessage.innerHeight();
+		var lastMessageHeight = newMessage.prev().innerHeight();
+
+		// Calculations
+		var sumHeights = clientHeight + scrollTop + newMessageHeight + lastMessageHeight;
+
+		if(sumHeights >= scrollHeight) {
+				messages.scrollTop(scrollHeight);
+		}
+
+}
+
 socket.on('connect', function() {
 		console.log('Connected to server!');
 });
@@ -19,6 +41,7 @@ socket.on('newMessage', function(message) {
 		});
 
 		jQuery('#messages').append(html);
+		scrollToBottom();
 
 });
 
@@ -33,6 +56,7 @@ socket.on('newLocationMessage', function(message) {
 		});
 
 		jQuery('#messages').append(html);
+		scrollToBottom();
 
 });
 
